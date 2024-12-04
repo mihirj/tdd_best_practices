51fe03f927f8a464c101277210b1952a0b4dfa8c
MIhir Joshi <68311242+mihirj@users.noreply.github.com>
2024-12-05 00:24:19 +0530

    [Feature] Add user model tests (#1)
    [Feature] Add user model tests

---
+++ test/src/authentication/data/user_model_test.dart
@@ -0,0 +1,80 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd_practice/core/utils/typedef.dart';
+import 'package:tdd_practice/src/authentication/data/user_models.dart';
+import 'package:tdd_practice/src/authentication/domain/entities/user.dart';
+
+import '../../../fixtures/fixture_reader.dart';
+
+void main() {
+  const tModel = UserModel.empty();
+
+  test('should be a subclass of [User] entity', () {
+    //   Act
+
+    //   Assert
+    expect(tModel, isA<User>());
+  });
+
+  final tJson = fixture('user.json');
+  final tMap = jsonDecode(tJson) as DataMap;
+
+  group('fromMap)', () {
+    test('should return a [UserModel] with the right data', () {
+      //   Act
+      final result = UserModel.fromMap(tMap);
+
+      // Assert
+      expect(result, equals(tModel));
+    });
+  });
+
+  group('fromJson)', () {
+    test('should return a [UserModel] with the right data', () {
+      //   Act
+      final result = UserModel.fromJson(tJson);
+
+      // Assert
+      expect(result, equals(tModel));
+    });
+  });
+
+  group('toMap)', () {
+    test('should return a [Map] with the right data', () {
+      //   Act
+      final result = tModel.toMap();
+
+      // Assert
+      expect(result, equals(tMap));
+    });
+  });
+
+  group('toJson)', () {
+    test('should return a [JSON] String with the right data', () {
+      // Arrange
+      final tJson = jsonEncode({
+        "id": "1",
+        "createdAt": "_empty.createdAt",
+        "name": "_empty.name",
+        "avatar": "_empty.avatar"
+      });
+
+      //   Act
+      final result = tModel.toJson();
+
+      // Assert
+      expect(result, equals(tJson));
+    });
+  });
+
+  group('copyWith)', () {
+    test('should return a [UserModel] with different data', () {
+      //   Act
+      final result = tModel.copyWith(name: 'Mihir');
+
+      // Assert
+      expect(result.name, equals('Mihir'));
+    });
+  });
+}
