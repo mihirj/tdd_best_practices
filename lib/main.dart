a035e9478edb53e4967152cee37b169ba9799814
mihirj <mhrjsh1@gmail.com>
2024-12-29 09:37:28 +0530

    feat: add dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tdd_practice/core/services/injection_container.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await init();
   runApp(const MainApp());
 }
 
